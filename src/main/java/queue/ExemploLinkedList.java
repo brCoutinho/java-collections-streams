415e70bd63d83844dc48d33dfcebfc020f90f937
Thiago Coutinho <thcoutinho1997@gmail.com>
2021-04-13 14:57:14 -0300

    Queue

---
+++ src/main/java/queue/ExemploLinkedList.java
@@ -0,0 +1,24 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExemploLinkedList {
+    public static void main(String[] args) {
+        Queue<String> filaBanco = new LinkedList<>();
+        filaBanco.add("João");
+        filaBanco.add("Otávio");
+        filaBanco.add("Felipe");
+        filaBanco.add("Valdemir");
+        System.out.println(filaBanco);
+
+        var clienteASerAtendido = filaBanco.poll();
+
+        System.out.println(clienteASerAtendido);
+        System.out.println(filaBanco);
+
+        var primeiroCliente = filaBanco.peek();
+        System.out.println(primeiroCliente);
+        
+    }
+}
